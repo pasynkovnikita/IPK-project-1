90427c0adfa8e16c665909e117cf246b57203205
pasynkovnikita <pasynkov.n.d@gmail.com>
2023-03-18 22:04:24 +0100

    feat: adds connection over tcp

--- client.c
+++ client.c
@@ -51,9 +51,69 @@ void parse_args(int argc, char **argv, char **host, char **port, char **mode) {
     }
 }
 
-int main (int argc, char * argv[]) {
+// function to handle tcp connection
+void tcp(char *host, char *port, char *mode) {
+    // Create socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR: socket");
+        exit(1);
+    }
+
+    // Get address info
+    struct hostent *server;
+    struct sockaddr_in server_address;
+    server = gethostbyname(host);
+
+    bzero((char *) &server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    bcopy((char *) server->h_addr, (char *) &server_address.sin_addr.s_addr, server->h_length);
+    server_address.sin_port = htons(atoi(port));
+
+    // Send request to server
+    if (connect(sockfd, (const struct sockaddr *) &server_address, sizeof(server_address)) != 0) {
+        perror("ERROR: connect");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send request to server
+    int bytestx, bytesrx, n;
+    char buf[BUFSIZE];
+    bzero(buf, BUFSIZE);
+//    fgets(buf, BUFSIZE, stdin);
+
+    while (strcmp(buf, "BYE\n") != 0) {
+        // clear buffer
+        bzero(buf, BUFSIZE);
+
+        // Get next line of input
+        fgets(buf, BUFSIZE, stdin);
+
+        bytestx = send(sockfd, buf, strlen(buf), 0);
+        if (bytestx < 0)
+            perror("ERROR in sendto");
+
+        // clear buffer
+        bzero(buf, BUFSIZE);
+
+        // Receive response from server
+        bytesrx = recv(sockfd, buf, BUFSIZE, 0);
+        if (bytesrx < 0)
+            perror("ERROR in recvfrom");
+
+        printf("Message from server: %s", buf);
+    }
+
+    // Close socket
+    close(sockfd);
+}
+
+
+int main(int argc, char *argv[]) {
     char *host, *port, *mode;
     parse_args(argc, argv, &host, &port, &mode);
-
+    if (strcmp(mode, "tcp") == 0) {
+        tcp(host, port, mode);
+    }
     return 0;
 }
