88525729eb035fd1286be0256fd442c47b394b0c
pasynkovnikita <pasynkov.n.d@gmail.com>
2023-03-21 13:16:03 +0100

    feat: changes bzero and bcopy to memset and memcpy to work on windows

--- client.c
+++ client.c
@@ -168,9 +168,9 @@ void udp(char *host, char *port) {
     struct sockaddr_in server_address;
     server = gethostbyname(host);
 
-    bzero((char *) &server_address, sizeof(server_address));
+    memset((char *) &server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
-    bcopy((char *) server->h_addr, (char *) &server_address.sin_addr.s_addr, server->h_length);
+    memcpy((char *) &server_address.sin_addr.s_addr, (char *) server->h_addr, server->h_length);
     server_address.sin_port = htons(atoi(port));
 
     // send request to server
